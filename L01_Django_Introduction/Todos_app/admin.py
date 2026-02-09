cf9f41fe34a2754a944020bbbc6d499c68859444
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2026-02-09 06:43:25 +0200

    Delete L01_Django_Introduction/Todos_app/admin.py

--- L01_Django_Introduction/Todos_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from djangoProject.todos import models
-
-
-# Register your models here.
-@admin.register(models.Tasks)
-class TaskAdmin(admin.ModelAdmin):
-    pass
